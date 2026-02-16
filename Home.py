74b84da0d8a9d5f5990852a356ab210c52d41197
Alexei <alexei.caminade@edu.devinci.fr>
2026-02-16 18:19:32 +0100

    modified:   Home.py

--- Home.py
+++ Home.py
@@ -161,7 +161,7 @@ carousel_html = f"""
         content: "";
         position: absolute;
         top: 0; left: 0; width: 100%; height: 100%;
-        background: rgba(0, 0, 0, 0.6); /* Filtre sombre */
+        background: rgba(0, 0, 0, 0); /* Filtre sombre */
         z-index: -1;
     }}
 
